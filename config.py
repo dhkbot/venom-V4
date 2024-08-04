cdb9b253e36962d28d224ac72b5e97bf1d543100
dhkbot <146471840+dhkbot@users.noreply.github.com>
2024-08-04 23:21:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6899087450:AAGQLHNdsAY9wCd8vB_Kc-tUsvkkIAYJD9Y")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@dev_kukdeja")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("5588464519")
